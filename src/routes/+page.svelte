441274bdd5941ed3adf9d32505e024972d5c8762
lucaaluke <luca2.verde@mail.polimi.it>
2026-03-25 17:40:26 +0100

    create clickscounter component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,13 @@
 <script>
-    console.log("Hello World");
-    let count = $state(0);
-        let doubled = $derived(count * 2);
+    import ClicksCounter from "$lib/components/ClicksCounter.svelte";
+    import ProgressiveSum from "$lib/components/ProgressiveSum.svelte";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button onclick={() => count++}>
-    Clicks: {count}
-</button>
-
-<span>Doubled: {doubled}</span>
+<ClicksCounter />
+<ProgressiveSum />
 
 <style>
     h1{
