7acb71a34f81fc2ee41fca28170df359731ac1d9
lucaaluke <luca2.verde@mail.polimi.it>
2026-03-25 17:29:01 +0100

    added state and derived

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <script>
     console.log("Hello World");
+    let count = $state(0);
+        let doubled = $derived(count * 2);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={() => count++}>
+    Clicks: {count}
+</button>
+
+<span>Doubled: {doubled}</span>
 
 <style>
     h1{
